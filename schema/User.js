07951373f795f1b154ec340d84e2acc5c0e2dd7a
Dashrath <sandip9028162859@gmail.com>
2023-07-09 11:25:33 +0530

    adding  user schema

---
+++ schema/User.js
@@ -0,0 +1,31 @@
+const mongoose =require("mongoose")
+const {isEmail}=require('validator');
+
+// creating user schema
+const userSchema= new mongoose.Schema({
+
+    createdPollIds:{
+        type:Array,
+    },
+    participatedPollIds:{
+        type:Array,
+    },
+    email:{
+        
+        type:String,
+        required:true,
+        unique:true,
+        validate: [isEmail,"please Enter valid Email"]
+
+    },
+    password:{
+        type:String,
+        required:true,  
+        minLength:[4,"Password  minimum lenght should be 4"] 
+    }
+
+
+})
+
+ module.exports =mongoose.model('user',userSchema);
+
