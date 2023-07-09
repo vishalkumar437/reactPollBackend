896c8cd1e3b1827445e6165701cf7f96f46cd4f3
Dashrath <sandip9028162859@gmail.com>
2023-07-09 15:28:49 +0530

    fix user login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const {Router}= require("express");
-const {userSignUpPost} =require("../Controller/userHandler")
+const {userSignUpPost, userLoginPost} =require("../Controller/userHandler")
 
 const router= Router();
 
 router.post("/createUser",userSignUpPost);
+route.post("/userLogin",userLoginPost);
 
 module.exports=router;
