71f8443c54f1d27216b1f452cb47d51d69ca0ee5
vishal kumar <vishalkumar437@gmail.com>
2023-07-09 16:00:46 +0530

    Final Commit

--- Controller/userHandler.js
+++ Controller/userHandler.js
@@ -22,10 +22,11 @@ module.exports.userSignUpPost = (req, res) => {
 module.exports.userLoginPost = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-
+  console.log(email,password)
   userModule
     .findOne({ email })
     .then((result) => {
+      console.log(result)
       if (result.password === password) {
         res.status(200).send({
           id: result._id,
@@ -33,11 +34,16 @@ module.exports.userLoginPost = (req, res) => {
           result,
         });
       }
+      else{
+        res.status(401).send({
+          msg: "Invalid Credentials",
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
-      res.status(401).send({
-        msg: "invalid credintials",
+      res.status(400).send({
+        msg: err,
       });
     });
 };
