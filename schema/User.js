073058b36771814a702729c2c5c4ffb379cfe157
Dashrath <sandip9028162859@gmail.com>
2023-07-09 12:14:29 +0530

    controller

--- schema/User.js
+++ schema/User.js
@@ -22,6 +22,10 @@ const userSchema= new mongoose.Schema({
         type:String,
         required:true,  
         minLength:[4,"Password  minimum lenght should be 4"] 
+    },
+
+    profileUrl:{
+        type:String,
     }
 
 
