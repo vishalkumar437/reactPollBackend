24ded35cac3dbf5c2db0194d5a48438ff55470b1
Dashrath <sandip9028162859@gmail.com>
2023-07-11 13:11:33 +0530

    user sync with polls

--- Controller/pollHandler.js
+++ Controller/pollHandler.js
@@ -1,32 +1,71 @@
 const express =require("express");
 const router = express.Router();
 const poll = require("../schema/Poll");
+const userModule = require("../schema/User");
 const mongo = require("mongoose");
 
 module.exports.create_poll = (req,res)=> {
+    console.log("create poll route hit");
     let question = req.body.Question;
     let options = req.body.Options;
     let category = req.body.Category;
-    let creator_Id = req.body.creatorId;
+    let creatorId = req.body.creatorId;
     const Poll = new poll({
         Question: question,
         Options: options,
         Category: category,
-        creatorId:creator_Id
+        creatorId:creatorId
     })
-    Poll.save()
+    
+
+    //finding creator of poll and then saving poll's id in  creators createdpoll array
+    //and saving Poll
+     userModule
+    .findById(creatorId).then((user)=>{
+        console.log("user found successfull",user);
+        //creating poll
+          Poll.save()
     .then(result=>{
+
+        // after poll created take its id and attach to creator
+        let newcreatedPollIds=[result._id.toString(),...user.createdPollIds]
+        console.log(user._id.toString(),typeof(user._id));
+        console.log("new creaters poll array is ",newcreatedPollIds);
+        userModule.updateOne({_id:user._id.toString()},{$set:{createdPollIds:newcreatedPollIds}}).then((result)=>{
+            console.log("creator- and- poll attachment successfull");
+            return;
+        }).catch((err)=>{
+            console.log("Creator and Poll not synced ");
+            res.status(500).json({
+                error:err
+            })    
+        })
+
         console.log("Poll Created Successfully");
         res.status(201).json({
+            msg:"Poll Created successfully",
             newPoll: result
         })
     })
     .catch(err=>{
-        console.log("Error Occured");
+        console.log("failed to create poll");
         res.status(500).json({
             error:err
         })
+        return;
     });
+
+
+    }).catch((err)=>{
+        console.log("No such user exist how poll will be created by such user")
+        
+        res.status(500).json({
+            msg:"User not locatable",
+            error:err,
+        })
+        return;
+    })
+  
 }
 
 module.exports.submit_poll = (req,res)=>{
